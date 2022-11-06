27e48017612380c0e7c2834f8e6924aadca88f6c
SnO2WMaN <me@sno2wman.net>
2022-11-06 16:55:35 +0900

    without overlay

--- flake.nix
+++ flake.nix
@@ -1,5 +1,37 @@
 {
-  outputs = {self} @ inputs: {
-    overlays.default = import ./overlay.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    {
+      overlays.default = import ./overlay.nix;
+    }
+    // (
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlays.default
+            ];
+          };
+        in {
+          mkCorepack = {
+            nodejs ? (pkgs: pkgs.nodejs),
+            pm ? "pnpm",
+          }: (
+            pkgs.mkCorepack {
+              inherit pm;
+              nodejs = nodejs pkgs;
+            }
+          );
+        }
+      )
+    );
 }
